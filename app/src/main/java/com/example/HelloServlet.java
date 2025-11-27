cc366137d45c0f561fec112f108f3c36e0ba76ee
foulolee <newultra82@naver.com>
2025-11-27 10:01:45 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/"})
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>안녕, K8S + ArgoCD + GitHub Actions!</h1>");
+        out.println("</body></html>");
+    }
+}
