4fb4dc8917ba29e7abc7548943bf1a771824ba11
foulolee <newultra82@naver.com>
2025-12-05 17:41:35 +0900

    Update HelloServlet.java

--- app/src/main/java/com/example/appweb/HelloServlet.java
+++ app/src/main/java/com/example/appweb/HelloServlet.java
@@ -3,11 +3,12 @@ package com.example.appweb;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class HelloServlet extends HttpServlet {
 
