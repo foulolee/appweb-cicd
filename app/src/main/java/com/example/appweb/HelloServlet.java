0c75d38c480aa35e5238ac07c4838ff2741169ab
foulolee <newultra82@naver.com>
2026-04-16 18:29:11 +0900

    Update HelloServlet.java

--- app/src/main/java/com/example/appweb/HelloServlet.java
+++ app/src/main/java/com/example/appweb/HelloServlet.java
@@ -29,6 +29,6 @@ public class HelloServlet extends HttpServlet {
         out.println("<p>Server Hostname: " + hostname + "</p>");
         out.println("<p>Server IP: " + hostaddr + "</p>");
         out.println("</body></html>");  
-        out.println("쓰발");
+        out.println("쓰발ㅇㅇㅇ");
     }
 }
